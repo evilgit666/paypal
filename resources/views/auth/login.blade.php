3fc7dd62e837dfacc055a68a58500030d54595c4
To Nguyen <32400888+binhnguyen1998822@users.noreply.github.com>
2019-05-02 14:42:50 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Pricing - S-Unlock</title>
+    <title>PayPal</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- App favicon -->
     <link rel="shortcut icon" href="assets/images/favicon.ico">
